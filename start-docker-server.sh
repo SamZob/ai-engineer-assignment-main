da1e133c312a9cdf26f8d20ff859f0280701220f
SamZob <zobiri.samia@gmail.com>
2024-05-01 10:25:18 +0200

    add image name

--- start-docker-server.sh
+++ start-docker-server.sh
@@ -1,10 +1,16 @@
-# we will use this file to start the docker server
-# and test your application.
-# please do not modify this file or any relevant files
-# unless you know what you are doing.
+#!/bin/bash
+
+# Define your image name
+IMAGE_NAME="code_snippet_generator"
+
+# Check if the .env file exists
 if [ ! -f .env ]; then
     echo "Please create a .env file, see .env.example for reference"
     exit 1
 fi
+
+# Build the Docker image
 docker build -t $IMAGE_NAME .
+
+# Run the Docker container
 docker run --rm -p 8000:8000 --env-file .env $IMAGE_NAME
